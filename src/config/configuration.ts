955778eb811a5029b426cb410ff368cf0af39442
Gabriel Pérez <gabriel@garox.org>
2026-03-27 17:23:19 -0400

    feat: remove unused env

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -3,7 +3,6 @@ import z from "zod";
 const configurationSchema = z.object({
     PORT: z.number().default(3000),
     DATABASE_URL: z.url(),
-    HARDCODED_API_KEY: z.string().min(10)
 });
 
 export type ConfigurationSchema = z.infer<typeof configurationSchema>;
