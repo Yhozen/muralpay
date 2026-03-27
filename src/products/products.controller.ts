1e2238745152a77c3ae4f94f8b98427b08a3949d
Gabriel Pérez <gabriel@garox.org>
2026-03-27 17:27:21 -0400

    add decorator

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,13 +1,16 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '../auth/auth.guard';
 import { ProductsService } from './products.service';
+import { User, type UserEntity } from 'src/users/users.decorators';
 
 @Controller('products')
 @UseGuards(AuthGuard)
 export class ProductsController {
     constructor(private readonly productService: ProductsService) { }
     @Get()
-    async getProducts(): Promise<string> {
+    async getProducts(@User() user: UserEntity): Promise<string> {
+        console.log({ user })
+
         return this.productService.getTest()
     }
 }
