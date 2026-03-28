81106c5e284e62377b362c656702204fd07e7238
Gabriel Pérez <gabriel@garox.org>
2026-03-27 22:59:45 -0400

    feat: migrate to bun

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -3,6 +3,7 @@ import z from 'zod';
 const configurationSchema = z.object({
   PORT: z.number().default(3000),
   DATABASE_URL: z.url(),
+  MURALPAY_API_KEY: z.string(),
 });
 
 export type ConfigurationSchema = z.infer<typeof configurationSchema>;
